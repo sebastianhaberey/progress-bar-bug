ade4de43b684c45a3a623de6baf48d4144b7924c
Sebastian Haberey <sebastian.haberey@netgo.de>
2023-12-15 10:19:21 +0100

    reproduced progressbar bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterOutlet } from "@angular/router";
+import { MatProgressBarModule } from "@angular/material/progress-bar";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [CommonModule, RouterOutlet, MatProgressBarModule],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'progress-bar-bug';
+  title = "progress-bar-bug";
 }
